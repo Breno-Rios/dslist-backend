1db707b40cbd3d3d8175244cb2547aac97860451
Breno-Rios <brenooliveirarios@gmail.com>
2025-01-21 20:56:08 -0300

    GetById

--- src/main/java/com/rios/dslist_backend/services/GameService.java
+++ src/main/java/com/rios/dslist_backend/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.rios.dslist_backend.dto.GameDTO;
 import com.rios.dslist_backend.dto.GameMinDTO;
 import com.rios.dslist_backend.entities.Game;
 import com.rios.dslist_backend.repositories.GameRepository;
@@ -15,6 +17,14 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+
+	}
+
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 
 		List<Game> result = gameRepository.findAll();
